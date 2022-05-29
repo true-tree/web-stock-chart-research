108c79af5d046d76d0c026b7505f83a672ad72bb
daeYoungH <dy.hwangs@gmail.com>
2022-05-29 23:59:22 +0900

    feat: klineChart

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TooltipKLineChart from "./chart/TooltipKLineChart";
+
+export default function App() {
+  return (
+    <div className="app">
+      <TooltipKLineChart />
+    </div>
+  );
+}
---
+++ src/chart/TooltipKLineChart.jsx
@@ -0,0 +1,103 @@
+import React, { PureComponent } from "react";
+import { init, dispose } from "klinecharts";
+import generatedKLineDataList from "../utils/generatedKLineDataList";
+import Layout from "../Layout";
+
+function getTooltipOptions(
+  candleShowType,
+  candleShowRule,
+  technicalIndicatorShowRule
+) {
+  return {
+    candle: {
+      tooltip: {
+        showType: candleShowType,
+        showRule: candleShowRule,
+        labels: ["open：", "close：", "diff"],
+        values: (kLineData) => {
+          const change =
+            ((kLineData.close - kLineData.open) / kLineData.open) * 100;
+          return [
+            { value: kLineData.open.toFixed(2) },
+            { value: kLineData.close.toFixed(2) },
+            {
+              value: `${change.toFixed(2)}%`,
+              color: change < 0 ? "#EF5350" : "#26A69A",
+            },
+          ];
+        },
+      },
+    },
+    technicalIndicator: {
+      tooltip: {
+        showRule: technicalIndicatorShowRule,
+      },
+    },
+  };
+}
+
+const rules = [
+  { key: "always", text: "总是显示" },
+  { key: "follow_cross", text: "跟随十字光标" },
+  { key: "none", text: "不显示" },
+];
+
+export default class TooltipKLineChart extends PureComponent {
+  state = {
+    candleShowType: "standard",
+    candleShowRule: "always",
+    technicalIndicatorShowRule: "always",
+  };
+
+  componentDidMount() {
+    const { candleShowType, candleShowRule, technicalIndicatorShowRule } =
+      this.state;
+    this.kLineChart = init("tooltip-k-line");
+    this.kLineChart.createTechnicalIndicator("MA", false, {
+      id: "candle_pane",
+    });
+    this.kLineChart.createTechnicalIndicator("KDJ", false, { height: 80 });
+    this.kLineChart.setStyleOptions(
+      getTooltipOptions(
+        candleShowType,
+        candleShowRule,
+        technicalIndicatorShowRule
+      )
+    );
+    this.kLineChart.applyNewData(generatedKLineDataList());
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    const { candleShowType, candleShowRule, technicalIndicatorShowRule } =
+      this.state;
+    if (
+      prevState.candleShowType !== candleShowType ||
+      prevState.candleShowRule !== candleShowRule ||
+      prevState.technicalIndicatorShowRule !== technicalIndicatorShowRule
+    ) {
+      this.kLineChart.setStyleOptions(
+        getTooltipOptions(
+          candleShowType,
+          candleShowRule,
+          technicalIndicatorShowRule
+        )
+      );
+    }
+  }
+
+  componentWillUnmount() {
+    dispose("tooltip-k-line");
+  }
+
+  render() {
+    return (
+      <Layout title="test">
+        <div
+          id="tooltip-k-line"
+          className="k-line-chart"
+          style={{ height: "500px" }}
+        />
+      </Layout>
+    );
+  }
+}
